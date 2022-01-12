e1a4596aa70dc5d280a357d604cfa0ea96653e2f
Cody <Mason.Cody.M@gmail.com>
2022-01-11 23:44:13 -0500

    Adapted TR3 to use TRUtil; still needs a modified `update.xml`.

--- TombRaiderIII/Autosplitter.cs
+++ TombRaiderIII/Autosplitter.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using LiveSplit.Model;
-using LiveSplit.UI.Components.AutoSplit;
+﻿using System.Diagnostics.CodeAnalysis;
+using TRUtil;
 
 namespace TR3
 {
-    /// <summary>
-    ///     The game's level and demo values.
-    /// </summary>
+    /// <summary>The game's level and demo values.</summary>
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     internal enum Level : uint
     {
@@ -41,135 +36,19 @@ namespace TR3
         AllHallows = 20
     }
 
-    /// <summary>
-    ///     Implementation of <see cref="IAutoSplitter"/> for an <see cref="AutoSplitComponent"/>'s use.
-    /// </summary>
-    internal class Autosplitter : IAutoSplitter, IDisposable
+    /// <summary>Implementation of <see cref="ClassicAutosplitter"/>.</summary>
+    internal sealed class Autosplitter : ClassicAutosplitter
     {
-        private const int LevelCount = 20; // This considers Lara's Home as 0 and includes the bonus level.
-
-        private readonly List<Level> _completedLevels = new List<Level>(LevelCount);
-
-        internal readonly ComponentSettings Settings = new ComponentSettings();
-        internal GameData GameData = new GameData();
-
-        /// <summary>A constructor that primarily exists to handle events/delegations.</summary>
-        public Autosplitter() => GameData.OnGameFound += Settings.SetGameVersion;
-
-        /// <summary>
-        ///     Determines the IGT.
-        /// </summary>
-        /// <param name="state"><see cref="LiveSplitState"/> passed by LiveSplit</param>
-        /// <returns>IGT as a <see cref="TimeSpan"/> if available, otherwise <see langword="null"/></returns>
-        public TimeSpan? GetGameTime(LiveSplitState state)
+        /// <summary>A constructor that primarily exists to handle events/delegations and set static values.</summary>
+        public Autosplitter()
         {
-            // Check that IGT is ticking.
-            uint currentLevelTicks = GameData.LevelTime.Current;
-            uint oldLevelTicks = GameData.LevelTime.Old;
-            if (currentLevelTicks - oldLevelTicks == 0)
-                return null;
+            Settings = new ComponentSettings();
 
-            // TR3's IGT ticks during globe level selection; the saved end-level IGT is unaffected, thus the overall FG IGT is also unaffected.
-            // If a runner is watching LS's IGT, this may confuse them, despite it being a non-issue for the level/FG IGT.
-            // To prevent the ticks from showing in LS, we use the fact that LevelComplete isn't reset to 0 until the next level is loaded.
-            Level currentLevel = GameData.Level.Current;
-            bool oldLevelComplete = GameData.LevelComplete.Old;
-            bool currentLevelComplete = GameData.LevelComplete.Current;
-            bool levelStillComplete = oldLevelComplete && currentLevelComplete;
-            if (_completedLevels.Contains(currentLevel) && levelStillComplete)
-                return null;
+            LevelCount = 20;
+            CompletedLevels.Capacity = LevelCount;
 
-            // Sum the current and completed levels' IGT.
-            double currentLevelTime = GameData.LevelTimeAsDouble(currentLevelTicks);
-            double finishedLevelsTime = GameData.SumCompletedLevelTimes(_completedLevels);
-            return TimeSpan.FromSeconds(currentLevelTime + finishedLevelsTime);
-        }
-
-        /// <summary>
-        ///     Determines if IGT should be paused when the game is quit or <see cref="GetGameTime"/> returns <see langword="null"/>
-        /// </summary>
-        /// <param name="state"><see cref="LiveSplitState"/> passed by LiveSplit</param>
-        /// <returns><see langword="true"/> if IGT should be paused, <see langword="false"/> otherwise</returns>
-        public bool IsGameTimePaused(LiveSplitState state) => true;
-
-        /// <summary>
-        ///     Determines if the timer should split.
-        /// </summary>
-        /// <param name="state"><see cref="LiveSplitState"/> passed by LiveSplit</param>
-        /// <returns><see langword="true"/> if the timer should split, <see langword="false"/> otherwise</returns>
-        public bool ShouldSplit(LiveSplitState state)
-        {
-            // Determine if the player is on the correct level to split; if not, we stop.
-            Level currentLevel = GameData.Level.Current;
-            bool onCorrectLevelToSplit = !_completedLevels.Contains(currentLevel);
-            if (!onCorrectLevelToSplit)
-                return false;
-
-            // Deathrun
-            bool laraJustDied = GameData.Health.Old > 0 && GameData.Health.Current == 0;
-            if (Settings.Deathrun && laraJustDied)
-                return true;
-            
-            // FG & IL/Section
-            bool levelJustCompleted = !GameData.LevelComplete.Old && GameData.LevelComplete.Current;
-            return levelJustCompleted;
-        }
-
-        /// <summary>
-        ///     Determines if the timer should reset.
-        /// </summary>
-        /// <param name="state"><see cref="LiveSplitState"/> passed by LiveSplit</param>
-        /// <returns><see langword="true"/> if the timer should reset, <see langword="false"/> otherwise</returns>
-        public bool ShouldReset(LiveSplitState state)
-        {
-            /* It is hypothetically reasonable to use _fullGameFarthestLevel to reset
-             * if the player loads into a level ahead of their current level.
-             * However, considering a case where a runner accidentally loads an incorrect
-             * save after dying, it's clear that this should be avoided.
-             */
-            return GameData.PickedPassportFunction.Current == 2;
-        }
-
-        /// <summary>
-        ///     Determines if the timer should start.
-        /// </summary>
-        /// <param name="state"><see cref="LiveSplitState"/> passed by LiveSplit</param>
-        /// <returns><see langword="true"/> if the timer should start, <see langword="false"/> otherwise</returns>
-        public bool ShouldStart(LiveSplitState state)
-        {
-            uint oldLevelTime = GameData.LevelTime.Old;
-            uint currentLevelTime = GameData.LevelTime.Current;
-            uint currentPickedPassportFunction = GameData.PickedPassportFunction.Current;
-            bool oldTitleScreen = GameData.TitleScreen.Old;
-
-            // Perform new game logic first, since it is the only place where FG should start.
-            bool levelTimeJustStarted = oldLevelTime == 0 && currentLevelTime != 0 && currentLevelTime < 50;
-            bool newGameStarted = levelTimeJustStarted && currentPickedPassportFunction == 1;
-            if (newGameStarted)
-                return true;
-
-            return !Settings.FullGame && levelTimeJustStarted && !oldTitleScreen;
-        }
-
-        /// <summary>
-        ///     On <see cref="LiveSplitState.OnStart"/>, updates values.
-        /// </summary>
-        public void OnStart() => _completedLevels.Clear();
-
-        /// <summary>
-        ///     On <see cref="LiveSplitState.OnSplit"/>, updates values.
-        /// </summary>
-        public void OnSplit() => _completedLevels.Add(GameData.Level.Current);
-
-        /// <summary>
-        ///     On <see cref="LiveSplitState.OnUndoSplit"/>, updates values.
-        /// </summary>
-        public void OnUndoSplit() => _completedLevels.RemoveAt(_completedLevels.Count - 1);
-        
-        public void Dispose() 
-        {
-            GameData.OnGameFound -= Settings.SetGameVersion;
-            GameData = null;
+            Data = new GameData();
+            Data.OnGameFound += Settings.SetGameVersion;
         }
     }
 }
--- TombRaiderIII/GameData.cs
+++ TombRaiderIII/GameData.cs
@@ -1,113 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
 using LiveSplit.ComponentUtil;
+using TRUtil;
 
 namespace TR3
 {       
-    /// <summary>
-    ///     The supported game versions.
-    /// </summary>
+    /// <summary>The supported game versions.</summary>
     internal enum GameVersion
     {
         Int,       // From Steam
         JpCracked  // No-CD cracked TR3 from JP Gold bundle release
     }
 
-    /// <summary>
-    ///     The game's watched memory addresses.
-    /// </summary>
-    internal class GameData
+    /// <summary>Manages the game's watched memory values for <see cref="Autosplitter"/>'s use.</summary>
+    internal sealed class GameData : ClassicGameData
     {
-        private const int IGTTicksPerSecond = 30;
-        private const int FirstLevelTimeAddress = 0x6D2326;  // Valid for all supported game versions.
-        private const int LevelSaveStructSize = 0x33;
-
-        private static readonly MemoryWatcherList Watchers = new MemoryWatcherList
+        /// <summary>A constructor that primarily exists to set/modify static values/objects.</summary>
+        internal GameData()
         {
-            new MemoryWatcher<bool>(new DeepPointer(0x2A1C58)) { Name = "TitleScreen" },
-            new MemoryWatcher<bool>(new DeepPointer(0x233F54)) { Name = "LevelComplete" },
-            new MemoryWatcher<Level>(new DeepPointer(0xC561C)) { Name = "Level" },
-            new MemoryWatcher<uint>(new DeepPointer(0x2D27CF)) { Name = "LevelTime" },
-            new MemoryWatcher<uint>(new DeepPointer(0x226458)) { Name = "PickedPassportFunction" },
-            new MemoryWatcher<short>(new DeepPointer(0x22640C)) { Name = "Health" }
-        };
+            VersionHashes.Add("4044dc2c58f02bfea2572e80dd8f2abb", (uint)GameVersion.Int);
+            VersionHashes.Add("66404f58bb5dbf30707abfd245692cd2", (uint)GameVersion.JpCracked);
 
-        private Process _game;
-        private GameVersion _version;
-
-        public delegate void GameFoundDelegate(GameVersion? version);
-        public GameFoundDelegate OnGameFound;
-
-        /// <summary>
-        ///     Indicates if the game is on the title screen (main menu).
-        /// </summary>
-        /// <remarks>
-        ///     Goes back to 0 during demos.
-        /// </remarks>
-        public MemoryWatcher<bool> TitleScreen => (MemoryWatcher<bool>)Watchers["TitleScreen"];
-
-        /// <summary>
-        ///     Indicates if the current <see cref="Level"/> is finished.
-        /// </summary>
-        /// <remarks>
-        ///     1 while an end-level stats screen is active.
-        ///     Remains 1 through FMVs that immediately follow a stats screen, i.e., until the next level starts.
-        ///     Before most end-level in-game cutscenes, the value changes from 0 to 1 then back to 0 immediately.
-        ///     Otherwise, the value is 0.
-        /// </remarks>
-        public MemoryWatcher<bool> LevelComplete => (MemoryWatcher<bool>)Watchers["LevelComplete"];
-
-        /// <summary>
-        ///     Gives the value of the last active level.
-        /// </summary>
-        /// <remarks>
-        ///     For TR3, level order is player-chosen, thus the value may not match the order of play.
-        /// </remarks>
-        public MemoryWatcher<Level> Level => (MemoryWatcher<Level>)Watchers["Level"];
-
-        /// <summary>
-        ///     Gives the IGT value for the current level.
-        /// </summary>
-        public MemoryWatcher<uint> LevelTime => (MemoryWatcher<uint>)Watchers["LevelTime"];
-
-        /// <summary>
-        ///     Indicates the passport function chosen by the user.
-        /// </summary>
-        /// <remarks>
-        ///     0 if <c>Load Game</c> was picked.
-        ///     Changes to 1 when choosing <c>New Game</c> or <c>Save Game</c>.
-        ///     The value stays 1 until the inventory is reopened.
-        ///     The value is also 1 during the first FMV if you pick <c>New Game</c> from Lara's Home.
-        ///     The value is always 2 when using the <c>Exit To Title</c> or <c>Exit Game</c> pages.
-        ///     Anywhere else the value is 0.
-        /// </remarks>
-        public MemoryWatcher<uint> PickedPassportFunction => (MemoryWatcher<uint>)Watchers["PickedPassportFunction"];
-
-        /// <summary>
-        ///     Lara's current HP.
-        /// </summary>
-        /// <remarks>
-        ///     Max HP is 1000. When it hits 0, Lara dies.
-        /// </remarks>
-        public MemoryWatcher<short> Health => (MemoryWatcher<short>)Watchers["Health"];
-
-        /// <summary>
-        ///     Sets <see cref="GameData"/> addresses based on <paramref name="version"/>.
-        /// </summary>
-        /// <param name="version"><see cref="GameVersion"/> for which addresses should be assigned</param>
-        private static void SetAddresses(GameVersion version)
+            ProcessSearchNames.Add("tomb3");
+            
+            // Valid for all supported game versions.
+            FirstLevelTimeAddress = 0x6D2326;
+            LevelSaveStructSize = 0x33;
+        }
+        
+        protected override void SetAddresses(uint version)
         {
-            switch (version)
+            switch ((GameVersion)version)
             {
                 case GameVersion.Int:
                     Watchers.Clear();
                     Watchers.Add(new MemoryWatcher<bool>(new DeepPointer(0x2A1C58)) { Name = "TitleScreen"});
                     Watchers.Add(new MemoryWatcher<bool>(new DeepPointer(0x233F54)) { Name = "LevelComplete"});
-                    Watchers.Add(new MemoryWatcher<Level>(new DeepPointer(0xC561C)) { Name = "Level"});
+                    Watchers.Add(new MemoryWatcher<uint>(new DeepPointer(0xC561C)) { Name = "Level"});
                     Watchers.Add(new MemoryWatcher<uint>(new DeepPointer(0x2D27CF)) { Name = "LevelTime"});
                     Watchers.Add(new MemoryWatcher<uint>(new DeepPointer(0x226458)) { Name = "PickedPassportFunction"});
                     Watchers.Add(new MemoryWatcher<short>(new DeepPointer(0x22640C)) { Name = "Health"});
@@ -115,7 +43,7 @@ namespace TR3
                 case GameVersion.JpCracked:
                     Watchers.Add(new MemoryWatcher<bool>(new DeepPointer(0x2A1C60)) { Name = "TitleScreen"});
                     Watchers.Add(new MemoryWatcher<bool>(new DeepPointer(0x233F5C)) { Name = "LevelComplete"});
-                    Watchers.Add(new MemoryWatcher<Level>(new DeepPointer(0xC561C)) { Name = "Level"});
+                    Watchers.Add(new MemoryWatcher<uint>(new DeepPointer(0xC561C)) { Name = "Level"});
                     Watchers.Add(new MemoryWatcher<uint>(new DeepPointer(0x2D27CF)) { Name = "LevelTime"});
                     Watchers.Add(new MemoryWatcher<uint>(new DeepPointer(0x226458)) { Name = "PickedPassportFunction"});
                     Watchers.Add(new MemoryWatcher<short>(new DeepPointer(0x22640C)) { Name = "Health"});
@@ -123,115 +51,6 @@ namespace TR3
                 default:
                     throw new ArgumentOutOfRangeException(nameof(version), version, null);
             }
-        }
-
-        /// <summary>
-        ///     Converts level time ticks to a double representing time elapsed.
-        /// </summary>
-        public static double LevelTimeAsDouble(uint ticks) => (double)ticks / IGTTicksPerSecond;
-
-        /// <summary>
-        ///     Sums completed levels' times.
-        /// </summary>
-        /// <returns>The sum of completed levels' times as a double.</returns>
-        /// <remarks>
-        ///     Because TR3's level order is variable and we must maintain compatibility with Section and NG+ runs,
-        ///     we read addresses based on <c>_completedLevels</c>.
-        /// </remarks>
-        public double SumCompletedLevelTimes(IEnumerable<Level> completedLevels)
-        {
-            uint finishedLevelsTicks = completedLevels
-                .TakeWhile(completedLevel => completedLevel != Level.Current)
-                .Select(completedLevel => ((int)completedLevel - 1) * LevelSaveStructSize)
-                .Select(levelOffset => (IntPtr)(FirstLevelTimeAddress + levelOffset))
-                .Aggregate<IntPtr, uint>(0, (ticks, levelAddress) => ticks + _game.ReadValue<uint>(levelAddress));
-
-            return LevelTimeAsDouble(finishedLevelsTicks);
-        }
-
-        /// <summary>
-        ///     Updates <see cref="GameData"/> and its addresses' values.
-        /// </summary>
-        /// <returns>
-        ///     <see langword="true"/> if game data was able to be updated, <see langword="false"/> otherwise
-        /// </returns>
-        public bool Update()
-        {
-            try
-            {
-                if (_game == null || _game.HasExited)
-                {
-                    if (!SetGameProcessAndVersion())
-                        return false;
-
-                    SetAddresses(_version);
-                    OnGameFound.Invoke(_version);
-                    _game.EnableRaisingEvents = true;
-                    _game.Exited += (s, e) => OnGameFound.Invoke(null);
-                    return true;
-                }
-
-                Watchers.UpdateAll(_game);
-
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        ///     If applicable, finds a <see cref="Process"/> running an expected <see cref="GameVersion"/>.
-        /// </summary>
-        /// <returns>
-        ///     <see langword="true"/> if values were set, <see langword="false"/> otherwise
-        /// </returns>
-        private bool SetGameProcessAndVersion()
-        {
-            var versionHashes = new Dictionary<string, GameVersion>
-            {
-                {"4044dc2c58f02bfea2572e80dd8f2abb", GameVersion.Int},
-                {"66404f58bb5dbf30707abfd245692cd2", GameVersion.JpCracked}
-            };
-
-            Process[] gameProcesses = Process.GetProcessesByName("tomb3");
-            foreach (Process process in gameProcesses)
-            {
-                string exePath = process?.MainModule?.FileName;
-                if (string.IsNullOrEmpty(exePath))
-                    break;
-
-                string md5Hash = GetMd5Hash(exePath);
-                if (!versionHashes.TryGetValue(md5Hash, out GameVersion version)) 
-                    break;
-
-                _version = version;
-                _game = process;
-                return true;
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        ///     Computes the MD5 hash and formats as a simple, lowercased string.
-        /// </summary>
-        /// <param name="exePath">The file to hash</param>
-        /// <returns>Lowercased, invariant string representing the MD5 <paramref name="exePath"/></returns>
-        private static string GetMd5Hash(string exePath)
-        {
-            string md5Hash;
-            using (var md5 = MD5.Create())
-            {
-                using (var stream = File.Open(exePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-                {
-                    var hash = md5.ComputeHash(stream);
-                    md5Hash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-                }
-            }
-
-            return md5Hash;
         }
     }
 }
