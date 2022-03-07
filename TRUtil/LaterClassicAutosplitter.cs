89289e5099c60d068a76ef79d4e9316a5c8b33f2
Cody <Mason.Cody.M@gmail.com>
2022-03-06 21:32:03 -0500

    Improved ShouldReset and ShouldStart for later classics.

--- TRUtil/LaterClassicAutosplitter.cs
+++ TRUtil/LaterClassicAutosplitter.cs
@@ -16,12 +16,25 @@ namespace TRUtil
         public override TimeSpan? GetGameTime(LiveSplitState state)
             => null;
 
-        public override bool ShouldReset(LiveSplitState state) 
-            => LaterClassicGameData.GfLevelComplete.Current == 0 && LaterClassicGameData.Loading.Current;
+        public override bool ShouldReset(LiveSplitState state)
+        {
+            bool loadingIntoMainMenu = LaterClassicGameData.GfLevelComplete.Current == 0 && LaterClassicGameData.Loading.Current;
+            // Checking the old level number ensures that someone Alt+F4-ing or otherwise opening the game (perhaps after a crash)
+            // will not cause ShouldReset to return true, losing their LiveSplit run progress.
+            // This works because when loading non-test/demo games, the level value initializes as 0 before the main menu load is called.
+            bool comingFromALevel = BaseGameData.Level.Old != 0;
+            return loadingIntoMainMenu && comingFromALevel;
+        }
 
-        public override bool ShouldStart(LiveSplitState state) 
-            => BaseGameData.Level.Current == 1 && !LaterClassicGameData.Loading.Current && LaterClassicGameData.Loading.Old;
-        
+        public override bool ShouldStart(LiveSplitState state)
+        {
+            bool loadingScreenJustEnded = !LaterClassicGameData.Loading.Current && LaterClassicGameData.Loading.Old;
+            // GfLevelComplete will only be 1 if New Game is clicked; loading a save does not set GfLevelComplete to that level's value.
+            // Thus, the GfLevelComplete check will prevent Starts from someone loading into the first level.
+            bool loadedIntoFirstLevelFromTheMainMenu = LaterClassicGameData.GfLevelComplete.Old == 1;
+            return loadingScreenJustEnded && loadedIntoFirstLevelFromTheMainMenu;
+        }
+
         /// <summary>On <see cref="LiveSplitState.OnStart"/>, updates values.</summary>
         public virtual void OnStart() => ProgressTracker.Clear();
 
