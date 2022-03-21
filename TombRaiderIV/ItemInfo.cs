fd1f41d1d62ff25406870c6c5dfb4943bc55728e
Cody <Mason.Cody.M@gmail.com>
2022-03-21 01:20:18 -0400

    Fixed space/tab inconsistency.

--- TombRaiderIV/ItemInfo.cs
+++ TombRaiderIV/ItemInfo.cs
@@ -4,110 +4,110 @@
     {
         internal struct PHD_3DPOS
         {
-	        long x_pos { get; }
-	        long y_pos { get; }
-	        long z_pos { get; }
-	        short x_rot { get; }
-	        short y_rot { get; }
-	        short z_rot { get; }
+            long x_pos { get; }
+            long y_pos { get; }
+            long z_pos { get; }
+            short x_rot { get; }
+            short y_rot { get; }
+            short z_rot { get; }
         };
         
-		internal struct PHD_VECTOR
-		{
-			long x { get; }
-			long y { get; }
-			long z { get; }
-		};				
-		
+        internal struct PHD_VECTOR
+        {
+            long x { get; }
+            long y { get; }
+            long z { get; }
+        };				
+        
         internal struct PCLIGHT
-		{
-			float x { get; }
-			float y { get; }
-			float z { get; }
-			float r { get; }
-			float g { get; }
-			float b { get; }
-			long shadow { get; }
-			float Inner { get; }
-			float Outer { get; }
-			float InnerAngle { get; }
-			float OuterAngle { get; }
-			float Cutoff { get; }
-			float nx { get; }
-			float ny { get; }
-			float nz { get; }
-			long ix { get; }
-			long iy { get; }
-			long iz { get; }
-			long inx { get; }
-			long iny { get; }
-			long inz { get; }
-			float tr { get; }
-			float tg { get; }
-			float tb { get; }
-			float rs { get; }
-			float gs { get; }
-			float bs { get; }
-			long fcnt { get; }
-			byte Type { get; }
-			byte Active { get; }
-			PHD_VECTOR rlp { get; }
-			long Range { get; }
-		};
+        {
+            float x { get; }
+            float y { get; }
+            float z { get; }
+            float r { get; }
+            float g { get; }
+            float b { get; }
+            long shadow { get; }
+            float Inner { get; }
+            float Outer { get; }
+            float InnerAngle { get; }
+            float OuterAngle { get; }
+            float Cutoff { get; }
+            float nx { get; }
+            float ny { get; }
+            float nz { get; }
+            long ix { get; }
+            long iy { get; }
+            long iz { get; }
+            long inx { get; }
+            long iny { get; }
+            long inz { get; }
+            float tr { get; }
+            float tg { get; }
+            float tb { get; }
+            float rs { get; }
+            float gs { get; }
+            float bs { get; }
+            long fcnt { get; }
+            byte Type { get; }
+            byte Active { get; }
+            PHD_VECTOR rlp { get; }
+            long Range { get; }
+        };
 
         internal struct ITEM_LIGHT
         {
-	        long r { get; }
-	        long g { get;  } 
-	        long b { get; }
-	        long ambient { get; }
-	        long rs { get; }
-	        long gs { get; }
-	        long bs { get; }
-	        long fcnt { get; }
-	        PCLIGHT_Array CurrentLights { get; }
-	        PCLIGHT_Array PrevLights { get; }
-	        long nCurrentLights { get; }
-	        long nPrevLights { get; }
-	        long room_number { get; }
-	        long RoomChange { get; }
-	        PHD_VECTOR item_pos { get; }
-	        void* pCurrentLights { get; }
-	        void* pPrevLights { get; }
+            long r { get; }
+            long g { get;  } 
+            long b { get; }
+            long ambient { get; }
+            long rs { get; }
+            long gs { get; }
+            long bs { get; }
+            long fcnt { get; }
+            PCLIGHT_Array CurrentLights { get; }
+            PCLIGHT_Array PrevLights { get; }
+            long nCurrentLights { get; }
+            long nPrevLights { get; }
+            long room_number { get; }
+            long RoomChange { get; }
+            PHD_VECTOR item_pos { get; }
+            void* pCurrentLights { get; }
+            void* pPrevLights { get; }
         };
-		
-		// C# Doesn't allow fixed buffer sizes for strictly non-composed primitive types.
-		internal struct PCLIGHT_Array
+        
+        // C# Doesn't allow fixed buffer sizes for strictly non-composed primitive types.
+        internal struct PCLIGHT_Array
         {
-			PCLIGHT light00 { get; }
-			PCLIGHT light01 { get; }
-			PCLIGHT light02 { get; }
-			PCLIGHT light03 { get; }
-			PCLIGHT light04 { get; }
-			PCLIGHT light05 { get; }
-			PCLIGHT light06 { get; }
-			PCLIGHT light07 { get; }
-			PCLIGHT light08 { get; }
-			PCLIGHT light09 { get; }
-			PCLIGHT light10 { get; }
-			PCLIGHT light11 { get; }
-			PCLIGHT light12 { get; }
-			PCLIGHT light13 { get; }
-			PCLIGHT light14 { get; }
-			PCLIGHT light15 { get; }
-			PCLIGHT light16 { get; }
-			PCLIGHT light17 { get; }
-			PCLIGHT light18 { get; }
-			PCLIGHT light19 { get; }
-			PCLIGHT light20 { get; }
+            PCLIGHT light00 { get; }
+            PCLIGHT light01 { get; }
+            PCLIGHT light02 { get; }
+            PCLIGHT light03 { get; }
+            PCLIGHT light04 { get; }
+            PCLIGHT light05 { get; }
+            PCLIGHT light06 { get; }
+            PCLIGHT light07 { get; }
+            PCLIGHT light08 { get; }
+            PCLIGHT light09 { get; }
+            PCLIGHT light10 { get; }
+            PCLIGHT light11 { get; }
+            PCLIGHT light12 { get; }
+            PCLIGHT light13 { get; }
+            PCLIGHT light14 { get; }
+            PCLIGHT light15 { get; }
+            PCLIGHT light16 { get; }
+            PCLIGHT light17 { get; }
+            PCLIGHT light18 { get; }
+            PCLIGHT light19 { get; }
+            PCLIGHT light20 { get; }
         }
 
-		internal struct item_flags_Array
+        internal struct item_flags_Array
         {
-			short item_flag00 { get; }
-			short item_flag01 { get; }
-			short item_flag02 { get; }
-			short item_flag03 { get; }
+            short item_flag00 { get; }
+            short item_flag01 { get; }
+            short item_flag02 { get; }
+            short item_flag03 { get; }
         }
 
         long floor { get; }
@@ -136,11 +136,11 @@
         item_flags_Array item_flags { get; }
         void* data { get; }
         PHD_3DPOS pos { get; }
-		ITEM_LIGHT il { get; }
-		ulong bitfield { get; }
-		ulong meshswap_meshbits { get; }
-		short draw_room { get; }
-		short TOSSPAD {  get; }
+        ITEM_LIGHT il { get; }
+        ulong bitfield { get; }
+        ulong meshswap_meshbits { get; }
+        short draw_room { get; }
+        short TOSSPAD {  get; }
 
         public ItemInfo(long floor, ulong touch_bits, ulong mesh_bits, short object_number, short current_anim_state, short goal_anim_state, short required_anim_state, short anim_number, short frame_number, short room_number, short next_item, short next_active, short speed, short fallspeed, short hit_points, ushort box_number, short timer, short flags, short shade, short trigger_flags, short carried_item, short after_death, ushort fired_weapon, item_flags_Array item_flags, void* data, PHD_3DPOS pos, ITEM_LIGHT il, ulong bitfield, ulong meshswap_meshbits, short draw_room, short TOSSPAD)
         {
