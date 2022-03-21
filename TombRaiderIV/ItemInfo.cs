07a2f128620c2818ea6837f66f24fcc2559bc375
Cody <Mason.Cody.M@gmail.com>
2022-03-21 02:04:13 -0400

    Made fields accessible.

--- TombRaiderIV/ItemInfo.cs
+++ TombRaiderIV/ItemInfo.cs
@@ -4,55 +4,55 @@
     {
         internal struct PHD_3DPOS
         {
-            long x_pos { get; }
-            long y_pos { get; }
-            long z_pos { get; }
-            short x_rot { get; }
-            short y_rot { get; }
-            short z_rot { get; }
+            public long x_pos { get; }
+            public long y_pos { get; }
+            public long z_pos { get; }
+            public short x_rot { get; }
+            public short y_rot { get; }
+            public short z_rot { get; }
         };
         
         internal struct PHD_VECTOR
         {
-            long x { get; }
-            long y { get; }
-            long z { get; }
+            public long x { get; }
+            public long y { get; }
+            public long z { get; }
         };				
         
         internal struct PCLIGHT
         {
-            float x { get; }
-            float y { get; }
-            float z { get; }
-            float r { get; }
-            float g { get; }
-            float b { get; }
-            long shadow { get; }
-            float Inner { get; }
-            float Outer { get; }
-            float InnerAngle { get; }
-            float OuterAngle { get; }
-            float Cutoff { get; }
-            float nx { get; }
-            float ny { get; }
-            float nz { get; }
-            long ix { get; }
-            long iy { get; }
-            long iz { get; }
-            long inx { get; }
-            long iny { get; }
-            long inz { get; }
-            float tr { get; }
-            float tg { get; }
-            float tb { get; }
-            float rs { get; }
-            float gs { get; }
-            float bs { get; }
-            long fcnt { get; }
-            byte Type { get; }
-            byte Active { get; }
-            PHD_VECTOR rlp { get; }
-            long Range { get; }
+            public float x { get; }
+            public float y { get; }
+            public float z { get; }
+            public float r { get; }
+            public float g { get; }
+            public float b { get; }
+            public long shadow { get; }
+            public float Inner { get; }
+            public float Outer { get; }
+            public float InnerAngle { get; }
+            public float OuterAngle { get; }
+            public float Cutoff { get; }
+            public float nx { get; }
+            public float ny { get; }
+            public float nz { get; }
+            public long ix { get; }
+            public long iy { get; }
+            public long iz { get; }
+            public long inx { get; }
+            public long iny { get; }
+            public long inz { get; }
+            public float tr { get; }
+            public float tg { get; }
+            public float tb { get; }
+            public float rs { get; }
+            public float gs { get; }
+            public float bs { get; }
+            public long fcnt { get; }
+            public byte Type { get; }
+            public byte Active { get; }
+            public PHD_VECTOR rlp { get; }
+            public long Range { get; }
         };
 
         internal struct ITEM_LIGHT
@@ -79,68 +79,68 @@
         // C# Doesn't allow fixed buffer sizes for strictly non-composed primitive types.
         internal struct PCLIGHT_Array
         {
-            PCLIGHT light00 { get; }
-            PCLIGHT light01 { get; }
-            PCLIGHT light02 { get; }
-            PCLIGHT light03 { get; }
-            PCLIGHT light04 { get; }
-            PCLIGHT light05 { get; }
-            PCLIGHT light06 { get; }
-            PCLIGHT light07 { get; }
-            PCLIGHT light08 { get; }
-            PCLIGHT light09 { get; }
-            PCLIGHT light10 { get; }
-            PCLIGHT light11 { get; }
-            PCLIGHT light12 { get; }
-            PCLIGHT light13 { get; }
-            PCLIGHT light14 { get; }
-            PCLIGHT light15 { get; }
-            PCLIGHT light16 { get; }
-            PCLIGHT light17 { get; }
-            PCLIGHT light18 { get; }
-            PCLIGHT light19 { get; }
-            PCLIGHT light20 { get; }
+            public PCLIGHT light00 { get; }
+            public PCLIGHT light01 { get; }
+            public PCLIGHT light02 { get; }
+            public PCLIGHT light03 { get; }
+            public PCLIGHT light04 { get; }
+            public PCLIGHT light05 { get; }
+            public PCLIGHT light06 { get; }
+            public PCLIGHT light07 { get; }
+            public PCLIGHT light08 { get; }
+            public PCLIGHT light09 { get; }
+            public PCLIGHT light10 { get; }
+            public PCLIGHT light11 { get; }
+            public PCLIGHT light12 { get; }
+            public PCLIGHT light13 { get; }
+            public PCLIGHT light14 { get; }
+            public PCLIGHT light15 { get; }
+            public PCLIGHT light16 { get; }
+            public PCLIGHT light17 { get; }
+            public PCLIGHT light18 { get; }
+            public PCLIGHT light19 { get; }
+            public PCLIGHT light20 { get; }
         }
 
         internal struct item_flags_Array
         {
-            short item_flag00 { get; }
-            short item_flag01 { get; }
-            short item_flag02 { get; }
-            short item_flag03 { get; }
+            public short item_flag00 { get; }
+            public short item_flag01 { get; }
+            public short item_flag02 { get; }
+            public short item_flag03 { get; }
         }
 
-        long floor { get; }
-        ulong touch_bits { get; }
-        ulong mesh_bits { get; }
-        short object_number { get; }
-        short current_anim_state { get; }
-        short goal_anim_state { get; }
-        short required_anim_state { get; }
-        short anim_number { get; }
-        short frame_number { get; }
-        short room_number { get; }
-        short next_item { get; }
-        short next_active { get; }
-        short speed { get; }
-        short fallspeed { get; }
-        short hit_points { get; }
-        ushort box_number { get; }
-        short timer { get; }
-        short flags { get; }
-        short shade { get; }
-        short trigger_flags { get; }
-        short carried_item { get; }
-        short after_death { get; }
-        ushort fired_weapon { get; }
-        item_flags_Array item_flags { get; }
-        void* data { get; }
-        PHD_3DPOS pos { get; }
-        ITEM_LIGHT il { get; }
-        ulong bitfield { get; }
-        ulong meshswap_meshbits { get; }
-        short draw_room { get; }
-        short TOSSPAD {  get; }
+        public long floor { get; }
+        public ulong touch_bits { get; }
+        public ulong mesh_bits { get; }
+        public short object_number { get; }
+        public short current_anim_state { get; }
+        public short goal_anim_state { get; }
+        public short required_anim_state { get; }
+        public short anim_number { get; }
+        public short frame_number { get; }
+        public short room_number { get; }
+        public short next_item { get; }
+        public short next_active { get; }
+        public short speed { get; }
+        public short fallspeed { get; }
+        public short hit_points { get; }
+        public ushort box_number { get; }
+        public short timer { get; }
+        public short flags { get; }
+        public short shade { get; }
+        public short trigger_flags { get; }
+        public short carried_item { get; }
+        public short after_death { get; }
+        public ushort fired_weapon { get; }
+        public item_flags_Array item_flags { get; }
+        public void* data { get; }
+        public PHD_3DPOS pos { get; }
+        public ITEM_LIGHT il { get; }
+        public ulong bitfield { get; }
+        public ulong meshswap_meshbits { get; }
+        public short draw_room { get; }
+        public short TOSSPAD {  get; }
 
         public ItemInfo(long floor, ulong touch_bits, ulong mesh_bits, short object_number, short current_anim_state, short goal_anim_state, short required_anim_state, short anim_number, short frame_number, short room_number, short next_item, short next_active, short speed, short fallspeed, short hit_points, ushort box_number, short timer, short flags, short shade, short trigger_flags, short carried_item, short after_death, ushort fired_weapon, item_flags_Array item_flags, void* data, PHD_3DPOS pos, ITEM_LIGHT il, ulong bitfield, ulong meshswap_meshbits, short draw_room, short TOSSPAD)
         {
