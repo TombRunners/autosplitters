c447b958fe2a2d5e5ac7b8470245d8d9d6a08828
Cody <Mason.Cody.M@gmail.com>
2022-11-22 20:37:29 -0500

    Added support for TR5's Japanese game version (no-CD). (#78)
    * Added support for JP no-cd version.
    
    * Bumped version.
    
    * Added entry to ProcessSearchNames.

--- TombRaiderV/ComponentSettings.cs
+++ TombRaiderV/ComponentSettings.cs
@@ -17,11 +17,13 @@ public sealed class ComponentSettings : LaterClassicComponentSettings
     public override void SetGameVersion(uint version)
     {
         const string digitalText = "Steam/GOG [TR5]";
+        const string jpNoCdText = "Japanese No-CD [TR5]";
         const string unknownText = "Unknown/Undetected";
 
         string versionText = (GameVersion) version switch
         {
             GameVersion.SteamOrGog => digitalText,
+            GameVersion.JapaneseNoCd => jpNoCdText,
             _ => unknownText
         };
         GameVersionLabel.Text = "Game Version: " + versionText;
