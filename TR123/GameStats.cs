04951edbc7c6ecd799349b25e55495f1b96ebeb7
Cody <Mason.Cody.M@gmail.com>
2024-02-27 22:19:35 -0500

    Improved GameStats docs and API.

--- TR123/GameStats.cs
+++ TR123/GameStats.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TR123;
 
-public struct GameStats(int levelCount)
+/// <summary>Stats related to a game with <paramref name="levelCount" /> levels.</summary>
+/// <param name="levelCount">Number of levels in the game</param>
+public readonly struct GameStats(int levelCount)
 {
     private List<LevelStats> LevelStatistics { get; } = new(levelCount);
 
-    public bool GameComplete { get; private set; } = false;
+    /// <summary>The number of levels in the game.</summary>
     public readonly int LevelCount = levelCount;
 
-    public readonly IEnumerable<LevelStats> LevelStats => LevelStatistics;
+    /// <summary><see langword="true" /> if all levels have been added into the backing list; <see langword="false" /> otherwise.</summary>
+    public bool GameComplete => LevelStatistics.Count == LevelCount;
 
+    public IEnumerable<LevelStats> LevelStats => LevelStatistics;
+
+    /// <summary>Adds <paramref name="stats" /> to the backing list.</summary>
+    /// <param name="stats">Stats to add</param>
     public void AddLevelStats(LevelStats stats)
     {
         if (GameComplete)
             return;
 
+        if (LevelStatistics.Select(static stats => stats.LevelNumber).Contains(stats.LevelNumber))
+            return;
+
         LevelStatistics.Add(stats);
-        if (LevelStatistics.Count == LevelCount)
-            GameComplete = true;
     }
 
-    public void RemoveLevelStats(LevelStats stats)
-    {
-        LevelStatistics.Remove(stats);
-        GameComplete = false;
-    }
+    /// <summary>Removes the most recently added <see cref="LevelStats" /> from the backing list.</summary>
+    public void RemoveLevelStats() => LevelStatistics.RemoveAt(LevelStatistics.Count - 1);
+
+    /// <summary>Clears the backing list of all <see cref="LevelStats" />.</summary>
+    public void Clear() => LevelStatistics.Clear();
 }
