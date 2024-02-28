e504205a3147677bb0986629bdb7161b87c86084
Cody <Mason.Cody.M@gmail.com>
2024-02-27 21:59:33 -0500

    Added more members to Game enum, added GameStats and LevelStats.

---
+++ TR123/GameStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace TR123;
+
+public struct GameStats(int levelCount)
+{
+    private List<LevelStats> LevelStatistics { get; } = new(levelCount);
+
+    public bool GameComplete { get; private set; } = false;
+    public readonly int LevelCount = levelCount;
+
+    public readonly IEnumerable<LevelStats> LevelStats => LevelStatistics;
+
+    public void AddLevelStats(LevelStats stats)
+    {
+        if (GameComplete)
+            return;
+
+        LevelStatistics.Add(stats);
+        if (LevelStatistics.Count == LevelCount)
+            GameComplete = true;
+    }
+
+    public void RemoveLevelStats(LevelStats stats)
+    {
+        LevelStatistics.Remove(stats);
+        GameComplete = false;
+    }
+}
