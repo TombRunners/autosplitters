8e91ba4bb9acf506e2f1a8ca292188243ace43a4
Makoto Aono <renam.aono@gmail.com>
2024-04-13 14:31:20 -0300

    Update Autosplitters.Tests/Properties/AssemblyInfo.cs
    Co-authored-by: Cody <Mason.Cody.M@gmail.com>

--- Autosplitters.Tests/Properties/AssemblyInfo.cs
+++ Autosplitters.Tests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Autosplitters.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
