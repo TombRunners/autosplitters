1cb415c1d4b54ec729780640bfb4e0f0d551bc41
Cody <Mason.Cody.M@gmail.com>
2024-11-18 18:15:19 -0500

    Add TR5 cutsceneless EXE recognition. (#111)
    * Added recognition for cutscene skipper mod (Steam/GOG only).
    
    * Bumped version and updated DLL.

--- TombRaiderV/Enums/Tr5Version.cs
+++ TombRaiderV/Enums/Tr5Version.cs
@@ -3,7 +3,8 @@
 /// <summary>The supported game versions.</summary>
 internal enum Tr5Version
 {
-    None          = 0,
-    SteamOrGog    = 1,
-    JapaneseNoCd  = 2,
+    None                   = 0,
+    SteamOrGog             = 1,
+    SteamOrGogCutsceneless = 2,
+    JapaneseNoCd           = 3,
 }
