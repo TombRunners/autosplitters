aeaea8aaa8f5b05c819e03f33c90203e800c2bae
Cody <Mason.Cody.M@gmail.com>
2025-02-16 15:06:05 -0500

    Added EGS version info.

--- TR456/Enums/GameVersion.cs
+++ TR456/Enums/GameVersion.cs
@@ -4,5 +4,5 @@ public enum GameVersion
 {
     None             = 0,
     Unknown          = 1,
-    PublicV10        = 2, // GOG v1.0, Steam 17156603
+    PublicV10        = 2, // GOG v1.0, Steam 17156603, EGS TRX2_250128_19221_WIN
 }
