6d1c31606e2850f9102f3d0e8e11c9a53c1f531b
Cody <Mason.Cody.M@gmail.com>
2025-03-02 18:30:52 -0500

    Bumped test version.

--- TR456/Properties/AssemblyInfo.cs
+++ TR456/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2025.0.3.0")]
-[assembly: AssemblyFileVersion("2025.0.3.0")]
+[assembly: AssemblyVersion("2025.0.4.0")]
+[assembly: AssemblyFileVersion("2025.0.4.0")]
