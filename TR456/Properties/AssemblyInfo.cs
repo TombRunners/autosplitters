786573eba766dc7548f126fe7f5bdb915f46d364
Cody <Mason.Cody.M@gmail.com>
2025-03-31 21:52:39 -0400

    Changed to minor version bump.

--- TR456/Properties/AssemblyInfo.cs
+++ TR456/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2025.1.0.4")]
-[assembly: AssemblyFileVersion("2025.1.0.4")]
+[assembly: AssemblyVersion("2025.1.1.0")]
+[assembly: AssemblyFileVersion("2025.1.1.0")]
