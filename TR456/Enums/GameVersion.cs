706f868d38ac9095cb8312e2d5183094b6dd937d
Cody <Mason.Cody.M@gmail.com>
2025-04-18 16:48:38 -0400

    Added hashes and version names for Patch 1.

--- TR456/Enums/GameVersion.cs
+++ TR456/Enums/GameVersion.cs
@@ -6,4 +6,5 @@ public enum GameVersion
     Unknown          = 1,
     GogV10           = 2, // GOG v1.0
     PublicV10        = 3, // GOG v1.0.0, Steam 17156603, EGS TRX2_250128_19221_WIN
+    PublicV10Patch1  = 4, // GOG v1.0.0_Patch_1, Steam 17983102, EGS TRX2_250404_20819_WIN
 }
