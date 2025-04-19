3715cdb9a0c6e52781797a69ff8c11f209bc7dd6
Cody <Mason.Cody.M@gmail.com>
2025-04-19 12:34:02 -0400

    Bumped for hotfix.

--- TR456/Properties/AssemblyInfo.cs
+++ TR456/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2025.2.0.0")]
-[assembly: AssemblyFileVersion("2025.2.0.0")]
+[assembly: AssemblyVersion("2025.2.0.1")]
+[assembly: AssemblyFileVersion("2025.2.0.1")]
