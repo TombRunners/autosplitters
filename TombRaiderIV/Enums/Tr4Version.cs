1296fa4d5b505a1a094cb17b4d590b097821e542
Cody <midge@midge.me>
2025-11-26 14:59:13 -0500

    Added versions and removed naming warnings.

--- TombRaiderIV/Enums/Tr4Version.cs
+++ TombRaiderIV/Enums/Tr4Version.cs
@@ -1,8 +1,12 @@
-﻿namespace TR4;
+﻿// ReSharper disable InconsistentNaming
+
+namespace TR4;
 
 /// <summary>The supported game versions.</summary>
 public enum Tr4Version
 {
-    SteamOrGog        = 1,
-    TheTimesExclusive = 2,
+    SteamOrGog            = 1,
+    SteamOrGog16x9        = 2, // Bytes at address 0xA9410 changed to 39 8E E3 (float value 1.7777, 16/9)
+    TheTimesExclusive     = 3,
+    TheTimesExclusive16x9 = 4, // Bytes at address 0xA93E0 changed to 39 8E E3 (float value 1.7777, 16/9)
 }
